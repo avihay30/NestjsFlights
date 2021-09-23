987cc1c4f726f2c8e682734163805a5280cb7f7a
Avihay-Laptop <avihay30@gmail.com>
2021-09-23 23:58:02 +0300

    removed imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { FlightsController } from './flights/flights.controller';
 import { Flights } from './flights/flights.entity';
 import { FlightsModule } from './flights/flights.module';
 
